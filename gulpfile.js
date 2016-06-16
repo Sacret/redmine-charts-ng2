7507aef4f43f01b68ff3a9b713a8ba28a4eb2645
Anastasiya Kozaeva <kozaeva_n@progforce.com>
2016-06-16 16:12:58 +0300

    Modify gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,73 @@
-"use strict";
+'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
+var del = require('del');
+var tsProject = ts.createProject('tsconfig.json');
 var paths = {
-    sass: './src/**/*.scss',
-    css: './public/css',
-    ts: './src/**/*.ts',
-    js: './public/js'
+    build: 'build',
+    src: 'src',
+    sass: '**/*.scss',
+    ts: '**/*.ts'
 };
+/**
+ * Remove build directory.
+ */
+gulp.task('clean', function (cb) {
+    del([paths.build], cb);
+});
+/**
+ * Compile TypeScript sources and create sourcemaps in build directory.
+ */
+gulp.task('compile', function () {
+    var tsResult = gulp.src(paths.src + '/' + paths.ts)
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject));
+    return tsResult.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.build));
+});
+/**
+ * Copy all resources that are not TypeScript files into build directory.
+ */
+gulp.task('resources', function () {
+    gulp.src([paths.src + '/**/*', '!' + paths.ts])
+        .pipe(gulp.dest(paths.build));
+});
+/**
+ * Copy all required libraries into build directory.
+ */
+gulp.task('libs', function () {
+    return gulp.src([
+        'es6-shim/es6-shim.min.js',
+        'systemjs/dist/system-polyfills.js',
+        'systemjs/dist/system.src.js',
+        'reflect-metadata/Reflect.js',
+        'rxjs/**',
+        'zone.js/dist/**',
+        '@angular/**'
+    ], { cwd: 'node_modules/**' }) /* Glob required here. */
+        .pipe(gulp.dest(paths.build + '/lib'));
+});
+/**
+ * Compile SCSS files
+ */
 gulp.task('sass', function () {
-    gulp.src(paths.sass)
+    gulp.src(paths.src + '/' + paths.sass)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(paths.css));
+        .pipe(gulp.dest(paths.build));
 });
-gulp.task('ts', function () {
-    var params = {
-        noImplicitAny: true,
-        out: 'app.js'
-    };
-    gulp.src(paths.ts)
-        .pipe(ts(params))
-        .pipe(gulp.dest(paths.js));
-});
+/**
+ * Watch project canges.
+ */
 gulp.task('watch', function () {
-    gulp.watch(paths.sass, ['sass']);
-    gulp.watch(paths.ts, ['ts']);
+    gulp.watch(paths.src + '/' + paths.sass, ['sass']);
+    gulp.watch(paths.src + '/' + paths.ts, ['compile']);
 });
-gulp.task('build', ['sass', 'ts']);
-gulp.task('default', ['build']);
+/**
+ * Build the project.
+ */
+gulp.task('build', ['compile', 'resources', 'libs', 'sass'], function () {
+    console.log('Building the project ...');
+});
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,37 +1,86 @@
+'use strict';
+
 import * as gulp from 'gulp';
 import * as sass from 'gulp-sass';
 import * as concat from 'gulp-concat';
+import * as sourcemaps from 'gulp-sourcemaps';
 import * as ts from 'gulp-typescript';
 import * as uglify from 'gulp-uglify';
+import * as tslint from 'gulp-tslint';
+import * as del from 'del';
+
+const tsProject = ts.createProject('tsconfig.json');
 
 const paths = {
-  sass: './src/**/*.scss',
-  css: './public/css',
-  ts: './src/**/*.ts',
-  js: './public/js'
+  build: 'build',
+  src: 'src',
+  sass: '**/*.scss',
+  ts: '**/*.ts'
 };
 
+/**
+ * Remove build directory.
+ */
+gulp.task('clean', (cb) => {
+  del([paths.build], cb);
+});
+
+/**
+ * Compile TypeScript sources and create sourcemaps in build directory.
+ */
+gulp.task('compile', () => {
+  const tsResult = gulp.src(paths.src + '/' + paths.ts)
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject));
+  return tsResult.js
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.build));
+});
+
+/**
+ * Copy all resources that are not TypeScript files into build directory.
+ */
+gulp.task('resources', () => {
+  gulp.src([paths.src + '/**/*', '!' + paths.ts])
+    .pipe(gulp.dest(paths.build))
+});
+
+/**
+ * Copy all required libraries into build directory.
+ */
+gulp.task('libs', () => {
+  return gulp.src([
+    'es6-shim/es6-shim.min.js',
+    'systemjs/dist/system-polyfills.js',
+    'systemjs/dist/system.src.js',
+    'reflect-metadata/Reflect.js',
+    'rxjs/**',
+    'zone.js/dist/**',
+    '@angular/**'
+  ], { cwd: 'node_modules/**' }) /* Glob required here. */
+    .pipe(gulp.dest(paths.build + '/lib'));
+});
+
+/**
+ * Compile SCSS files
+ */
 gulp.task('sass', () => {
-  gulp.src(paths.sass)
+  gulp.src(paths.src + '/' + paths.sass)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(paths.css));
+    .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('ts', () => {
-  const params = {
-    noImplicitAny: true,
-    out: 'app.js'
-  };
-  gulp.src(paths.ts)
-    .pipe(ts(params))
-    .pipe(gulp.dest(paths.js));
-});
-
+/**
+ * Watch project canges.
+ */
 gulp.task('watch', () => {
-  gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.ts, ['ts']);
+  gulp.watch(paths.src + '/' + paths.sass, ['sass'])
+  gulp.watch(paths.src + '/' + paths.ts, ['compile']);
 });
 
-gulp.task('build', ['sass', 'ts']);
-
-gulp.task('default', ['build']);
+/**
+ * Build the project.
+ */
+gulp.task('build', ['compile', 'resources', 'libs', 'sass'], () => {
+  console.log('Building the project ...')
+});
