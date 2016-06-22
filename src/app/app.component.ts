7b2efc4b8d25a6642e8be6480f046938d760e656
Anastasiya Kozaeva <kozaeva_n@progforce.com>
2016-06-22 17:53:16 +0300

    Modify login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './components/header.component';
 @Component({
   selector: 'my-app',
   directives: [LoginComponent, HeaderComponent],
-  template: '<header></header><h1>My Angular 2 App</h1><login></login>'
+  template: '<header></header><h1>Welcome!</h1><login></login>'
 })
 
 export class AppComponent { }
